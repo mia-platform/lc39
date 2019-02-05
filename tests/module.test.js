90579683139a81f90602defdfbc88d9d7c715dcf
Jacopo Andrea Giola <jacopo@giola.org>
2019-02-05 17:20:11 +0100

    Fix overriding of default options for testLaunch function
    The testLaunch function has the wrong order for the default
    option overriding during tests.

--- tests/module.test.js
+++ tests/module.test.js
@@ -15,3 +15,19 @@ test('Launch Fastify for testing', async assert => {
   await fastifyInstance.close()
   assert.end()
 })
+
+test('Launch Fastify for testing, overriding default values', async assert => {
+  const fastifyInstance = await launch('./tests/modules/correct-module', {
+    port: 9000,
+    logLevel: 'trace',
+  })
+  assert.ok(fastifyInstance)
+
+  const serverAddress = fastifyInstance.server.address()
+  assert.strictSame(serverAddress.port, 9000)
+  assert.strictSame(serverAddress.address, '127.0.0.1')
+  assert.strictSame(fastifyInstance.log.level, 'trace')
+
+  await fastifyInstance.close()
+  assert.end()
+})
