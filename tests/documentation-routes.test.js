5dc3ac6b450aa6ab992e94ddb79a131064ecd63c
Jacopo Andrea Giola <jacopo@giola.org>
2019-04-04 17:29:19 +0200

    Add tests for the swagger functionality

---
+++ tests/documentation-routes.test.js
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2019 Mia srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict'
+
+const { test } = require('tap')
+const launch = require('../lib/launch-fastify')
+
+test('Test Fastify creation with no prefix', async assert => {
+  const options = {
+    logLevel: 'silent',
+    port: 3000,
+  }
+
+  const fastifyInstance = await launch('./tests/modules/correct-module', options)
+
+  const jsonResponse = await fastifyInstance.inject({
+    method: 'GET',
+    url: '/documentation/json',
+  })
+
+  assert.strictSame(jsonResponse.statusCode, 200)
+  assert.strictSame(jsonResponse.headers['content-type'], 'application/json; charset=utf-8')
+
+  const textResponse = await fastifyInstance.inject({
+    method: 'GET',
+    url: '/documentation/',
+  })
+
+  assert.strictSame(textResponse.statusCode, 200)
+  assert.strictSame(textResponse.headers['content-type'], 'text/html; charset=UTF-8')
+
+  await fastifyInstance.close()
+  assert.end()
+})
+
+test('Test Fastify creation with custom prefix', async assert => {
+  const options = {
+    prefix: '/prefix/',
+    logLevel: 'silent',
+    port: 3000,
+  }
+
+  const fastifyInstance = await launch('./tests/modules/correct-module', options)
+
+  const jsonResponse = await fastifyInstance.inject({
+    method: 'GET',
+    url: '/documentation/json',
+  })
+
+  assert.strictSame(jsonResponse.statusCode, 200)
+
+  await fastifyInstance.close()
+  assert.end()
+})
---
+++ tests/swagger-definition.test.js
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2019 Mia srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict'
+
+const { test } = require('tap')
+const { name, description, version } = require('../package.json')
+const swaggerDefinition = require('../lib/swagger-definition')
+
+test('Test the default returned swagger definition', (assert) => {
+  const swagger = swaggerDefinition()
+
+  assert.strictSame(swagger, {
+    info: {
+      title: name,
+      description,
+      version,
+    },
+    consumes: ['application/json'],
+    produces: ['application/json'],
+  })
+
+  assert.end()
+})
+
+test('Test the default returned swagger definition', (assert) => {
+  const customDefintion = {
+    info: {
+      title: 'custom-title',
+      description: 'custom description for swagger',
+      version: 'v2019.01.01',
+    },
+  }
+  const swagger = swaggerDefinition(customDefintion)
+  assert.strictSame(swagger, customDefintion)
+  assert.end()
+})
